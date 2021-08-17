d10a651b76e4cccb4b2a77f66805505909c14ae9
William Daghouz <w.daghouz@gmail.com>
2021-08-17 23:55:23 +0200

    Created masthead-component.

---
+++ src/components/indexPage/masthead.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const Masthead: FC = () => {
+  return <div></div>
+}
+
+export default Masthead
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { PageProps } from 'gatsby'
 import React, { FC } from 'react'
 import Layout from '../components/layout'
+import Masthead from '../components/indexPage/masthead'
 
 const IndexPage: FC<PageProps> = () => {
   return (
     <Layout>
-      <div>Hello, world!</div>
+      <Masthead />
     </Layout>
   )
 }
