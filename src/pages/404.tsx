40df092400899d1a1ebd4f3a3ae0510767c29c87
William Daghouz <w.daghouz@gmail.com>
2021-08-18 16:31:12 +0200

    Created 404-page-component.

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React, { FC, Fragment } from 'react'
+import { PageProps } from 'gatsby'
+
+const NotFoundPage: FC<PageProps> = () => {
+  return <Fragment></Fragment>
+}
+
+export default NotFoundPage
