fbc8c5b32a28c4a7b8c40d3e0e0aa89e3b550ddd
William Daghouz <w.daghouz@gmail.com>
2021-08-18 14:25:26 +0200

    Modified masthead-component.

--- src/components/indexPage/masthead.tsx
+++ src/components/indexPage/masthead.tsx
@@ -1,17 +1,29 @@
 import React, { FC } from 'react'
-// import { useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 
 const Masthead: FC = () => {
+  const {
+    graphCmsIndexPageMasthead: { imageAlt },
+  } = useStaticQuery(graphql`
+    query IndexPageMastheadQuery {
+      graphCmsIndexPageMasthead {
+        imageAlt
+      }
+    }
+  `)
+
   return (
-    <StaticImage
-      src='https://media.graphcms.com/output=format:jpg/2vRigXbYTEOftZp6lstt'
-      alt='Light bulb standing on the floor, turned on and connected to a wall socket.'
-      loading='eager'
-      formats={['auto', 'webp', 'avif']}
-      placeholder='none'
-      layout='fullWidth'
-    />
+    <>
+      <StaticImage
+        src='https://media.graphcms.com/output=format:jpg/2vRigXbYTEOftZp6lstt'
+        alt={imageAlt}
+        loading='eager'
+        formats={['auto', 'webp', 'avif']}
+        placeholder='none'
+        layout='fullWidth'
+      />
+    </>
   )
 }
 
